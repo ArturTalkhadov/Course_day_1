08ade83e6e4b8fc27f08b304d6ccdc9e8e387eb0
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-03-17 07:09:31 +0300

    Create pizza_deliveries2.py

---
+++ Course_day_3/pizza_deliveries2.py
@@ -0,0 +1,22 @@
+print("Добро пожаловать в PYTHON доставку пиццы ")
+size=input("Выберите размер пиццы: S, M, L ")
+add_pipperoni=input("Будете дополнительно пиперони? Y или N ")
+extra_cheese=input("Добавить больше сыра в пиццу? Y или N ")
+bill=0
+if size=="S":
+    bill+=15
+elif size=="M":
+    bill+=20
+else:
+    bill+=25
+
+if add_pipperoni=="Y":
+    if size=="S":
+        bill+=2
+    else:
+        bill+3
+
+if extra_cheese=="Y":
+    bill+=1
+
+print("Стоимость вашей пиццы равна:$ ")
