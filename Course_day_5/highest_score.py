9d2667e52797b9cc8100e6122c414cca42b1a215
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-03-28 17:22:41 +0300

    Update highest_score.py

--- Course_day_5/highest_score.py
+++ Course_day_5/highest_score.py
@@ -3,7 +3,7 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 max_score = 0
-min_score = 0
+min_score = 100
 for score in student_scores:
     if score>=max_score:
         max_score=score
