9c1633ca8d651a0c4c2be2c48a0c228a31253f1f
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-03-30 23:38:21 +0300

    Update caesar_cipher.py

--- Course_day_8/caesar_cipher.py
+++ Course_day_8/caesar_cipher.py
@@ -1,6 +1,27 @@
+from art import logo
+
+def cript(plain_text, shift_amount):
+    secret=""
+    for ch in plain_text:
+        secret+=(alphabet[(alphabet.index(ch)+shift_amount)%(len(alphabet))])
+    print(f"The encoded text is {secret}")
+
+def decrypt(plain_text, shift_amount):
+    encod=""
+    for c in plain_text:
+        encod+=(alphabet[(alphabet.index(c)-shift_amount)%(len(alphabet))])
+    print(f"The encrypted text is {encod}")
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+if direction == "encode":
+    cript(text, shift)
+else:
+    decrypt(text, shift)
