0b7a49714faf68e6cdf06f2886adc10bd9025613
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-03-31 21:58:48 +0300

    Create main.py

---
+++ Course_day_9/main.py
@@ -0,0 +1,5 @@
+travel_log = {
+  "France": {"citi_visited": ["Paris", "Lille", "Dijon"]}, #["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+print(travel_log["France"]["citi_visited"])
