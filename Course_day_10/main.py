3bfe6db29dd5f362cc337897268de777a42c0888
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-04-04 22:29:09 +0300

    Create main.py

---
+++ Course_day_10/main.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    print(f_name.title())
+    print(l_name.title())
+
+
+f_name = input("input first name: ")
+l_name = input("input last name: ")
+format_name(f_name, l_name)
