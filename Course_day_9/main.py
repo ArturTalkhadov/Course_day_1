35078871dc4d1a15146f230ca7bbaf68515b2502
ArturTalkhadov <80581626+ArturTalkhadov@users.noreply.github.com>
2021-04-04 22:28:56 +0300

    Delete main.py

--- Course_day_9/main.py
+++
@@ -1,5 +0,0 @@
-travel_log = {
-  "France": {"citi_visited": ["Paris", "Lille", "Dijon"]}, #["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
-}
-print(travel_log["France"]["citi_visited"])
